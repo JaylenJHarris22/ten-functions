78073d74c23b5e0affa16f1d43fe052ad400d121
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-02-23 08:52:36 -0600

    Completed the ten functions.

--- solutions.js
+++ solutions.js
@@ -21,3 +21,70 @@
  * concat
  */
 
+function isTrue(input){
+    if(input === true){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function isFalse(input){
+    if(input === false){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function not(input){
+    return !input
+}
+
+function addOne (input){
+    let add = Number(input) + 1
+    return add
+}
+
+function isEven (input){
+    return input % 2 === 0
+}
+
+function isIdentical(input, otherInput){
+    if(input === otherInput){
+        return true;
+    }else{
+        return false
+    }
+}
+
+function isEqual(input, otherInput){
+    if (input == otherInput){
+        return true
+    }else{
+        return false
+    }
+}
+
+function or(input, otherInput){
+    if (input === 'hello' || otherInput === 'world'){
+        return 'hello'
+    }else if (input == true || otherInput == true){
+        return true
+    }else{
+        return false
+    }
+}
+function and (input, otherInput){
+    if (input === 'hello' || otherInput === 'world'){
+        return 'world'
+    }else if (input == true && otherInput == true){
+        return true
+    }else {
+        return false
+    }
+}
+
+function concat (input, otherInput){
+    return String(input) + String(otherInput)
+}
